05d18e4cf672a009469b530705f2d461d3620722
supriyabhor <supriyanaikwadi89@gmail.com>
2025-02-23 23:39:36 -0500

    fetch image data form db

--- src/pages/PhotoGallery.jsx
+++ src/pages/PhotoGallery.jsx
@@ -1,8 +1,17 @@
-
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function PhotoGallery() {
 
+    useEffect(() => {
+        async function getPhotoData() {
+            let res = await axios.get('http://localhost:3000/photo');
 
+            console.log(res.data);
+        }
+        getPhotoData(); 
+
+    }, [])
 
 
     return(

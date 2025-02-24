ed690d57506f2e5a48ce15f3dfffca4bb380d7ae
supriyabhor <supriyanaikwadi89@gmail.com>
2025-02-24 04:02:36 -0500

    Fetch images form db and visible in browser

--- src/pages/PhotoGallery.jsx
+++ src/pages/PhotoGallery.jsx
@@ -1,22 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function PhotoGallery() {
+  const [photos, setPhotos] = useState([]);
 
-    useEffect(() => {
-        async function getPhotoData() {
-            let res = await axios.get('http://localhost:3000/photo');
+  useEffect(() => {
+    async function getPhotoData() {
+      let res = await axios.get('http://localhost:3000/photo');
+      setPhotos(res.data); // Update the state with the received data
+    }
+    getPhotoData();
+  }, []);
 
-            console.log(res.data);
-        }
-        getPhotoData(); 
-
-    }, [])
-
-
-    return(
-        <>
-        <h1>Photo Gallery</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Photo Gallery</h1>
+      <ul>
+        {photos.map((photo, index) => (
+          <li key={index}>
+            <img src={photo.photoURI} alt="Photo" />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
